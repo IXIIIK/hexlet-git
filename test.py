5b83cc980ce3bc680a2ed57bb65a2a25216889a9
iliya sinitcyn <baner1337228@gmail.com>
2022-08-12 16:06:58 +0300

    learn __name__ == __main__

--- test.py
+++ test.py
@@ -1,11 +1,3 @@
-def fib(n):
-    f1 = 0
-    f2 = 1
+from test1.mylife import greet
 
-    for f in range(0, n):
-        f = f1 + f2
-        f2 += f1
-        print(f, f2)
-    return f2
-
-print(fib(10))
+greet('jack')
--- test1/mylife.py
+++ test1/mylife.py
@@ -1,31 +1,9 @@
+def greet(who):
+    print (f'hello {who}')
 
+def main():
+    greet('bob')
+    greet('marlin')
 
-
-'''
-    try:
-
-        a = input('Первое число: ')
-        b = input('второе число: ')
-        c = input('какое действие выпольнить?(+, -, *, /): ')
-
-        if c == '+':
-            print(int(a) + int(b))
-
-        if c == '-':
-            print(int(a) - int(b))
-
-        if c == '*':
-            print(int(a) * int(b))
-
-        if c == '/':
-            print(int(a) / int(b))
-
-        else:
-            print('вы не выбрали действие!')
-
-    except ZeroDivisionError:
-        print('нельзя делить на ноль!')
-
-    except ValueError:
-        print('кажеться что то из полей не число!')
-'''
+if __name__ == '__main__':
+    main()
